436647a1d4671952754dffdfb792def0c9ac71a7
Louis Lainé <darksioul6@gmail.com>
2016-01-23 17:52:31 +0100

    Adding tree/file structure. Writing constants of the app

---
+++ app/constants/constants.js
@@ -0,0 +1,9 @@
+/**
+ * Ce fichier contient les constantes permettant
+ * d'identifier les actions de l'application.
+ */
+export default {
+  CREATED_ACCOUNT: 'created account',
+  WITHDREW_FROM_ACCOUNT: 'withdrew from account',
+  DEPOSITED_INTO_ACCOUNT: 'deposited into account'
+}
