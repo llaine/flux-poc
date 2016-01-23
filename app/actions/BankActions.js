84fb15cbc2847307a8f8a0ab5ca431cccf91468e
Louis Lainé <darksioul6@gmail.com>
2016-01-23 17:55:02 +0100

    feat(actions): Adding basic actions for the app

---
+++ app/actions/BankActions.js
@@ -0,0 +1,40 @@
+import AppDispatcher from '../dispatchers/AppDispatcher';
+import bankConstants from '../constants/constants';
+
+const BankActions = {
+
+  /**
+   * Action de base, permettant de créer un compte
+   * ayant une valeur vide.
+   */
+  createAccount() {
+    AppDispatcher.dispatch({
+      type: bankConstants.CREATED_ACCOUNT,
+      amount: 0
+    });
+  },
+
+  /**
+   * Amount add in the account.
+   * @param amount
+   */
+  depositIntoAccount(amount) {
+    AppDispatcher.dispatch({
+      type: bankConstants.DEPOSITED_INTO_ACCOUNT,
+      amount
+    });
+  },
+
+  /**
+   * Amount to withdraw from the account
+   * @param amount
+   */
+  withdrawFromAccount(amount) {
+    AppDispatcher.dispatch({
+      type: bankConstants.WITHDREW_FROM_ACCOUNT,
+      amount
+    });
+  }
+};
+
+export default BankActions;
